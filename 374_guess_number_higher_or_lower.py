782072d73c4bf48ef1ebbfed027938f69ab9b1be
pallavipsap <32209226+pallavipsap@users.noreply.github.com>
2020-05-22 16:46:24 -0700

    Binary search on May 22 2020
    has many downward reviews

---
+++ 374_guess_number_higher_or_lower.py
@@ -0,0 +1,30 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num: int) -> int:
+#May 22 2020
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        # many downwards
+
+        # binary search: Time O(logN) Space: O(1)
+        # Consider A as an array of numbers 1 to N
+        # instead of writing A[mid]< or > target, we are using numbers -1, 0 , 1
+
+        # choose from 1 to n
+        # consider indexes in arrays are numbered from 1 to n
+
+        l = 1
+        h = n
+
+        # send the index ( mid ) to the guess API
+        while l <= h:
+            mid = (l + h) // 2
+            g = guess(mid)  # returns -1, 1, 0
+            print(g)
+            if g == 0:  # congrats correct number guessed
+                return mid
+            elif g == 1:  # number is higher than the mid, move right , target > A[mid]
+                l = mid + 1
+            else:
+                h = mid - 1
