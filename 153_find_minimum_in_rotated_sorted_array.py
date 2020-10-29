3d81c985ebc44d82319dce107e0cf0dd76472e54
pallavipsap <32209226+pallavipsap@users.noreply.github.com>
2020-10-28 21:23:33 -0700

    second method added

--- 153_find_minimum_in_rotated_sorted_array.py
+++ 153_find_minimum_in_rotated_sorted_array.py
@@ -1,12 +1,21 @@
 # Refer 33. Search in Rotated Sorted Array
 # Done on May 23 2020
 
+# Check both methods for clarity
+
 class Solution:
     def findMin(self, nums: List[int]) -> int:
 
-        # no duplcate elements
+        # Time : O(log n)
+        # Space : O(1)
+
+        # no duplicate elements == so no need to worry for nums[mid] = l , nums[mid] = r
         # we have to move towards unsorted part
-        # it is quite possible both parts are
+        # One part is always sorted
+
+        # Method 2:
+        # Check on right side 1st
+
         start = 0
         end = len(nums) - 1
         '''
@@ -36,6 +45,8 @@ class Solution:
         while start < end:
 
             mid = (start + end) // 2
+
+            # check right 1st*****
             if nums[mid] > nums[end]:  # right side of array unsorted [ 4 5 6 7 0 1 2] mid = 7, end = 2
                 start = mid + 1
             else:  # left side unsorted, nums[start]>nums[mid] [ 7 8 1 2 3 ] start = 7, mid = 1
@@ -46,4 +57,36 @@ class Solution:
         # basically the loop will break when start == end
         # same thing is done by the above code, the moment start=end, we return
         return nums[start]
+
+        # Method 1:
+        # go to unsorted side
+        # go to left side 1st****
+
+        '''
+
+        l = 0
+        h = len(nums) - 1
+        # minimum = 0
+
+        while l < h:
+            print('here')
+            mid = l + (h - l) // 2
+
+            # Check left 1st****
+            # left is unsorted, go to left to find min
+            if nums[l] > nums[mid]:
+                h = mid
+
+            elif nums[h] < nums[mid]:
+                l = mid + 1
+
+            # else:
+            #     return nums[l]
+
+        print(l)
+        return nums[l]  # for input [1]
+
+
+
+        '''
 
