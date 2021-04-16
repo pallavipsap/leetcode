8635204c4150f3cf549b96c27051262d366dbf2e
pallavipsap <32209226+pallavipsap@users.noreply.github.com>
2021-04-15 18:39:15 -0700

    done

--- 98_validate_binary_search_tree.py
+++ 98_validate_binary_search_tree.py
@@ -1,6 +1,6 @@
 # Pallavi Sapale
 # June 16, 2020
-# to understand inorder
+# to understand inorder 
 # Yet to confirm why prev = None, and not root
 # Yet to try third approach ( inf )
 # why prev is None
@@ -12,24 +12,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # global variable prev
+
     # use self.prev while using prev, this is not considered as a global variable
-    # prev local to this specific class
+    # prev local to this specific class, self suggests its local to a specific class
+    
 
     def isValidBST(self, root: TreeNode) -> bool:
+        
         # Method 2 : Recursive approach :
         # does not work in leetcode when prev is global
         # Time Complexity : O(n) == we visit all nodes exactly once
         # Space Complexity : O( height of tree) == O(n) worst case
-
+        
         # recursive does not work on leet code when prev used as global variable
-
+        
         self.prev = TreeNode()
         self.prev = None
         return self.inOrder(root)
+        
 
-    def inOrder(self, root):  # added for recursion
-
+    def inOrder(self, root): # added for recursion
+        
         if root == None: return True
         if self.inOrder(root.left) == False:  # if left subtree is not BST
             return False
@@ -39,14 +42,15 @@ class Solution:
             return False
         self.prev = root  # else we update prev as the root, and  ofcourse we change our root here at the bottom
 
-        return self.inOrder(root.right)  # explore right subtree if left subtree
+        return self.inOrder(root.right)  # explore right subtree if left subtree is a BST
 
-        # Method 1 : iterative approach : Works in leetcode
-        # Time Complexity : O(n) == we visit all nodes exactly once
-        # Space Complexity : O( height of tree)
+        # Method 1 : iterative approach : Works in leetcode -- detailed explanation
+        # Time Complexity : O(n) == we visit all nodes exactly once,
+        # Space Complexity : O( height of tree) -- this gives more clarity
+        # if tree is one sided only ( stick ).. O(n)
 
         '''
-        - Use in order traversal, because nodes are arranged in ascendign order, which is true for a valid BST
+        - Use in order traversal, because nodes are arranged in ascending order, which is true for a valid BST
         - set prev and root values to compare everytime
         - Remember, once you pop the node, you do not insert same node again
         Problems : 
@@ -86,3 +90,72 @@ class Solution:
         return True
 
         '''
+
+        # Method 1.1 : other way to do iterative approach
+        '''
+        stack = []
+        curr_val = float('-inf')
+        while root or len(stack)!= 0:
+            
+            while root:
+                stack.append(root) # append root, not val
+                root=root.left
+                
+            # pop before going ot right
+            root = stack.pop()
+            if root.val <= curr_val: # [1,1] should return false, hence <=
+                return False # if new value ( root value ) is less than curr value, return False
+            curr_val = root.val
+            root = root.right
+        
+        return True
+        '''
+
+        # other ways to do recursion
+
+        # Method 2.1 :  Recursion without flag condition
+        '''
+        self.flag = True
+        self.curr = float(-inf) # negative values also present so go for negative infinity
+        self.dfs(root)
+        return self.flag
+
+        def dfs(self,root):
+
+            if root == None:
+                return
+
+            self.dfs(root.left)
+            if root.val <= self.curr: # if next value is less than curr , flag = False
+                self.flag = False
+            else:
+                self.curr = root.val
+
+            self.dfs(root.right)
+        
+        '''   
+        # Method 2.3 :  Recursion with flag condition (to not call recursion further, and keep returning)
+        '''
+        self.flag = True
+        self.curr = float(-inf) # negative values also present so go for negative infinity
+        self.dfs(root)
+        return self.flag
+
+        def dfs(self,root):
+            
+            # recursion will keep going on, if Flas condition is not mentioned, this also gives correct output but once flag is False, further recursion calls are unnecessary
+            if root == None or self.flag == False: # this does not add more recursion calls to the stack. once flag is False, irrespective of root being None, we return
+                return
+            
+            self.dfs(root.left)
+            if root.val <= self.curr: # if next value is less than curr , flag = False
+                self.flag = False
+            else:
+                self.curr = root.val
+            
+            self.dfs(root.right)
+        '''
+       
+
+            
+        
